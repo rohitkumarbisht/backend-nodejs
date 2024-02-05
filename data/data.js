94c0dc1e4a69d697d912f3905d37114641c9136f
Vaishali Agrawal <vaishali.agrawal@magicsw.com>
2024-02-05 13:14:25 +0530

    PostgreSQL db connection and initial setup

---
+++ data/data.js
@@ -0,0 +1,44 @@
+const studentData = [
+  {
+    id: "1",
+    total_assessment: "11",
+    total_questions: "60",
+    percentage: "55%",
+    avg_test_time: "00:40:16",
+    total_test_time: "01:00:00",
+  },
+  {
+    id: "2",
+    total_assessment: "10",
+    total_questions: "40",
+    percentage: "45%",
+    avg_test_time: "00:40:16",
+    total_test_time: "01:00:00",
+  },
+  {
+    id: "3",
+    total_assessment: "11",
+    total_questions: "60",
+    percentage: "55%",
+    avg_test_time: "00:40:16",
+    total_test_time: "01:00:00",
+  },
+  {
+    id: "4",
+    total_assessment: "11",
+    total_questions: "60",
+    percentage: "55%",
+    avg_test_time: "00:40:16",
+    total_test_time: "01:00:00",
+  },
+  {
+    id: "5",
+    total_assessment: "11",
+    total_questions: "60",
+    percentage: "55%",
+    avg_test_time: "00:40:16",
+    total_test_time: "01:00:00",
+  },
+];
+
+module.exports = studentData;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const data = require("./data/data");
+const db = require("./config/db");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const userId = "1234";
+
+    const df = await db.fetchData(userId);
+
+    console.log("Dataframe:", df);
+
+    for (const col in df[0]) {
+      console.log(col);
+    }
+
+    console.log("DataFrame updated.");
+    res.json(df);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
