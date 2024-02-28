a16ef47b99d1b81862c3211b25c33beeb1cb5654
Shivam Rathi <shivam.rathi@magicsw.com>
2024-02-28 19:01:19 +0530

    Update startApplication.sh

--- Dependency_Scripts/startApplication.sh
+++ Dependency_Scripts/startApplication.sh
@@ -4,5 +4,5 @@ version=$(cat ${dir}/../buildNumber.txt)
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 619942913628.dkr.ecr.ap-south-1.amazonaws.com
 docker pull 619942913628.dkr.ecr.ap-south-1.amazonaws.com/dev-accelerators:learning-analytics-api-$version
-docker run -d -p 8081:8080 --name learning-analytics-api 619942913628.dkr.ecr.ap-south-1.amazonaws.com/dev-accelerators:learning-analytics-api-$version
+docker run -d -p 5000:5000 --name learning-analytics-api 619942913628.dkr.ecr.ap-south-1.amazonaws.com/dev-accelerators:learning-analytics-api-$version
 sleep 10
