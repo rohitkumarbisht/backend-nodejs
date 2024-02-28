246363d2a84d186a44f09326ecceb64ec82f509b
rohit.bisht <rohit.bisht@magicedtech.com>
2024-02-28 12:25:51 +0530

    Optimization for production

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const { getAssessmentWiseReport, getOverallProgress, getTeacherView } = require('../controller/dataController');
-const { jwt_secret_key, jwt_audience, jwt_issuer } = require('../config/config.js');
+const { ANALYTICS_REPORT, TEACHER_VIEW, OVERALL_PROGRESS, ASSESSMENT_WISE } = require('../config/config.js');
 const { CustomUnauthorizedError, CustomNotFoundError, CustomInternalServerError } = require('./error-handling/custom_error.js');
+const extractRoleFromToken  = require('../utils/extract_role.js');
+const extractUserIdFromToken = require('../utils/extract_user_id.js');
 
-router.get('/analytics-report/assessment-wise', async (req, res) => {
+router.get(`${ANALYTICS_REPORT}${ASSESSMENT_WISE}`, async (req, res) => {
   try {
     const assessment_id = req.query.assessment_id;
     const user_id = extractUserIdFromToken(req.headers.authorization);
@@ -24,7 +25,7 @@ router.get('/analytics-report/assessment-wise', async (req, res) => {
   }
 });
 
-router.get('/analytics-report/overall-progress', async (req, res) => {
+router.get(`${ANALYTICS_REPORT}${OVERALL_PROGRESS}`, async (req, res) => {
   try {
     const user_id = extractUserIdFromToken(req.headers.authorization);
     const data = await getOverallProgress(user_id);
@@ -36,13 +37,11 @@ router.get('/analytics-report/overall-progress', async (req, res) => {
       res.status(err.statusCode).json({ error: err.message });
     } else if (err instanceof CustomInternalServerError) {
       res.status(err.statusCode).json({ error: err.message });
-    } else {
-      res.status(200).json({ error: err })
     }
   }
 });
 
-router.get('/analytics-report/teacher-view', async (req, res) => {
+router.get(`${ANALYTICS_REPORT}${TEACHER_VIEW}`, async (req, res) => {
   try {
     extractRoleFromToken(req.headers.authorization)
     const data = await getTeacherView();
@@ -55,64 +54,10 @@ router.get('/analytics-report/teacher-view', async (req, res) => {
     } else if (err instanceof CustomInternalServerError) {
       res.status(err.statusCode).json({ error: err.message });
     } else {
+      console.log(err);
       res.status(200).json({ error: err })
     }
   }
 });
-
-function extractRoleFromToken(authorizationHeader){
-  const options = {
-    algorithms: ['HS256'],
-    ignoreExpiration: true,
-    issuer: jwt_issuer,
-    audience: jwt_audience
-  };
-
-  let role = ''
-  let token = ''
-
-  try {
-    token = authorizationHeader.split(' ')[1];
-  } catch (err) {
-    throw new CustomUnauthorizedError(`Unauthorized: token not found `);
-  }
-  const base64Key = Buffer.from(jwt_secret_key).toString('base64');
-  try {
-    const decodedToken = jwt.verify(token, base64Key, options);
-    if(decodedToken.roles=='ROLE_ADMIN' || decodedToken.roles=='ROLE_TEACHER'){
-      role = decodedToken.roles;}
-    else{
-      throw new err;
-    }
-  } catch (err) {
-    throw new CustomUnauthorizedError(`Unauthorized: User not having permision `);
-  }
-  return role;
-}
-
-function extractUserIdFromToken(authorizationHeader) {
-  const options = {
-    algorithms: ['HS256'],
-    ignoreExpiration: true,
-    issuer: jwt_issuer,
-    audience: jwt_audience
-  };
-  let user_id = ''
-  let token = ''
-  try {
-    token = authorizationHeader.split(' ')[1];
-  } catch (err) {
-    throw new CustomUnauthorizedError(`Unauthorized: token not found `);
-  }
-  const base64Key = Buffer.from(jwt_secret_key).toString('base64');
-  try {
-    const decodedToken = jwt.verify(token, base64Key, options);
-    user_id = decodedToken.jti;
-
-  } catch (err) {
-    throw new CustomUnauthorizedError(`Unauthorized: ${err} `);
-  }
-  return user_id;
-}
 
 module.exports = router;
