7c190b4a36798e974abdec6ae07f943112468db7
Shivam Rathi <shivam.rathi@magicsw.com>
2024-02-28 18:23:31 +0530

    Add startApplication

---
+++ Dependency_Scripts/startApplication.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dir=$(dirname "$0")
+version=$(cat ${dir}/../buildNumber.txt)
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 619942913628.dkr.ecr.ap-south-1.amazonaws.com
+docker pull 619942913628.dkr.ecr.ap-south-1.amazonaws.com/dev-accelerators:learning-analytics-api-$version
+docker run -d -p 8081:8080 --name learning-analytics-api 619942913628.dkr.ecr.ap-south-1.amazonaws.com/dev-accelerators:learning-analytics-api-$version
+sleep 10
