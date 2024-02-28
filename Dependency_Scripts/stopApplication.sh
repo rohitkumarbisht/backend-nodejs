3782cc7f4e47f530c69143107f7ab1186d4d0efc
Shivam Rathi <shivam.rathi@magicsw.com>
2024-02-28 19:37:20 +0530

    Update stopApplication.sh

--- Dependency_Scripts/stopApplication.sh
+++ Dependency_Scripts/stopApplication.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+container_id=$(docker ps -a | grep "learning-analytics-api" | awk '{print $1}')
+if [[ "$container_id" != "" ]]
+then
+        docker stop $container_id
+        docker rm $container_id
+else
+        echo "No container present"
+fi
 
-bash /home/ubuntu/la-api/Dependency_Scripts/stopapi.sh
+image_id=$(docker images | grep "learning-analytics-api" | awk '{print $3}')
+if [[ "$image_id" != "" ]]
+then
+        docker rmi -f $image_id
+else
+        echo "No image present"
+fi
